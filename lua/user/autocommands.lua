f6a6c402ba6594c4d636f550f88187463909b0e6
Logan(Luo) <logan.luo.x@gmail.com>
2022-08-10 19:14:18 +0800

    feat: restore cursor positon

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -45,3 +45,10 @@ vim.api.nvim_create_autocmd({ "TextYankPost" }, {
   end,
 })
 
+-- Restore cursor position
+vim.api.nvim_create_autocmd({ "BufReadPost" }, {
+    pattern = { "*" },
+    callback = function()
+        vim.api.nvim_exec('silent! normal! g`"zv', false)
+    end,
+})
