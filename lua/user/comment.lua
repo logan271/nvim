7a2a28a4291d78539eaa6280c1259441287898cd
Logan(Luo) <logan.luo.x@gmail.com>
2022-08-13 22:08:12 +0800

    chore: update comment plugin config

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -1,22 +1,22 @@
 local status_ok, comment = pcall(require, "Comment")
 if not status_ok then
-  return
+	return
 end
 
-comment.setup {
-  pre_hook = function(ctx)
-    local U = require "Comment.utils"
+-- https://github.com/numToStr/Comment.nvim
+comment.setup({
+	---Add a space b/w comment and the line
+	---@type boolean|fun():boolean
+	padding = true,
 
-    local location = nil
-    if ctx.ctype == U.ctype.block then
-      location = require("ts_context_commentstring.utils").get_cursor_location()
-    elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-      location = require("ts_context_commentstring.utils").get_visual_start_location()
-    end
+	---Whether the cursor should stay at its position
+	---NOTE: This only affects NORMAL mode mappings and doesn't work with dot-repeat
+	---@type boolean
+	sticky = true,
 
-    return require("ts_context_commentstring.internal").calculate_commentstring {
-      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-      location = location,
-    }
-  end,
-}
+	---Lines to be ignored while comment/uncomment.
+	---Could be a regex string or a function that returns a regex string.
+	---Example: Use '^$' to ignore empty lines
+	---@type string|fun():string
+	ignore = nil,
+})
